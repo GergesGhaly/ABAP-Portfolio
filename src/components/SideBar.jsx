87efe91f665c6ea778370da408a20f060c1398be
GergesGhaly <gerges.ghaly1@gmail.com>
2025-11-20 01:54:26 +0200

    set sidebarOpen init to  false

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,7 +7,7 @@ export default function SideBar() {
   const [openProjects, setOpenProjects] = useState(false);
   const [openProjectFiles, setOpenProjectFiles] = useState({});
   const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const toggleProjectFiles = (id) => {
     setOpenProjectFiles((prev) => ({

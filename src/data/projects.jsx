e567de1b4b26358a2bfe45fa8c5a278461979db5
GergesGhaly <gerges.ghaly1@gmail.com>
2025-12-15 13:10:15 +0200

    update zgg_alv_viewer proj

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,5 +1,6 @@
 import file1Content from "./code/ZGG_ALV_VIEWER.txt?raw";
 import file2Content from "./code/ZGG_ALV_VIEWER_DEF.txt?raw";
+import ZGG_ALV_VIEWER_Documentation from "./code/Documentation.txt?raw";
 
 export const projectsList = [
   {
@@ -9,6 +10,7 @@ export const projectsList = [
     files: [
       { id: "file1", name: "ZGG_ALV_VIEWER", content: file1Content },
       { id: "file2", name: "ZGG_ALV_VIEWER_DEF", content: file2Content },
+      { id: "file3", name: "Documentation", content: ZGG_ALV_VIEWER_Documentation },
     ],
   },
   // {
